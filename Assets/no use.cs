80b10197ccaa4b75982ff7efc68ad5fac33a0c5f
Changda Ma <126302481+changdama@users.noreply.github.com>
2025-01-24 15:07:38 -0500

    Update no use.cs

--- Assets/no use.cs
+++ Assets/no use.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class RotateAroundSelf : MonoBehaviour
 {
-    public float rotationSpeed = 1f; // 每秒旋转的角度
+    public float rotationSpeed = 1f; 
 
     void Update()
     {
-        // 绕y轴旋转
+       
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
     }
 }
